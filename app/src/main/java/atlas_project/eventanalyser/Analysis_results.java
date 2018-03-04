dbae51db2c6fb65c63f02372ba9f32b8c7f9bb91
potato_farmer <wildtangles@gmail.com>
2018-03-04 23:30:24 +0000

    added histogram name and bitmap/drawable fetch from firestore

--- app/src/main/java/atlas_project/eventanalyser/Analysis_results.java
+++ app/src/main/java/atlas_project/eventanalyser/Analysis_results.java
@@ -1,21 +1,32 @@
 package atlas_project.eventanalyser;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.Blob;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.w3c.dom.Document;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class Analysis_results extends AppCompatActivity {
     DocumentReference docRef;
@@ -36,7 +47,18 @@ public class Analysis_results extends AppCompatActivity {
                 if (task.isSuccessful()) {
                     DocumentSnapshot document = task.getResult();
                     if (document != null && document.exists()) {
-                        Log.i("Firestore Fetch:", "DocumentSnapshot data: " + document.getData());
+                        Map<String, Object> histograms = document.getData();
+
+                        Iterator histStore = histograms.entrySet().iterator();
+                        while (histStore.hasNext()){
+                            Map.Entry pair = (Map.Entry)histStore.next();
+                            String histogramStr = String.valueOf(pair.getKey()); //do something with the histogram title
+                            Log.i("Firestore Fetch Key:", "histogram name: " + histogramStr);
+                            byte[] decodedStr = Base64.decode(String.valueOf(pair.getValue()), Base64.DEFAULT);
+                            Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedStr, 0, decodedStr.length);
+                            Drawable histogramImg = new BitmapDrawable(getResources(), decodedByte); //do something with the histogram drawable
+                            histStore.remove();
+                        }
                     } else {
                         Log.w("Firestore Fetch:", "No such document");
                     }
