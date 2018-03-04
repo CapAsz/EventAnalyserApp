f6ebef9efa5b3b0c4d36cd0faa8b56e7dfb3c15f
potato_farmer <wildtangles@gmail.com>
2018-03-04 15:36:29 +0000

    demo firestore fetch

--- app/src/main/java/atlas_project/eventanalyser/Analysis_results.java
+++ app/src/main/java/atlas_project/eventanalyser/Analysis_results.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,6 +15,8 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import org.w3c.dom.Document;
+
 public class Analysis_results extends AppCompatActivity {
     DocumentReference docRef;
     private String key = "";
@@ -22,27 +25,25 @@ public class Analysis_results extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_analysis_results);
-        FirebaseApp.initializeApp(this);
-        FirebaseFirestore histograms = FirebaseFirestore.getInstance();
 
         key = getIntent().getStringExtra("HistKey");
-        //docRef = histograms.collection("imgStore").document(key);
+        docRef = FirebaseFirestore.getInstance().collection("imgStore").document(key);
 
-//        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot> () {
-//            @Override
-//            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-//                if (task.isSuccessful()) {
-//                    DocumentSnapshot document = task.getResult();
-//                    if (document != null && document.exists()) {
-//                        System.out.print("DocumentSnapshot data: " + document.getData());
-//                    } else {
-//                        System.out.print("No such document");
-//                    }
-//                } else {
-//                    System.out.print("get failed with " +task.getException());
-//                }
-//            }
-//        });
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot> () {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document != null && document.exists()) {
+                        Log.i("Firestore Fetch:", "DocumentSnapshot data: " + document.getData());
+                    } else {
+                        Log.w("Firestore Fetch:", "No such document");
+                    }
+                } else {
+                    Log.e("Firestore Fetch:", "get failed with " +task.getException());
+                }
+            }
+        });
 
         Button backButton = findViewById(R.id.back_button);
         backButton.setOnClickListener(new View.OnClickListener() {
